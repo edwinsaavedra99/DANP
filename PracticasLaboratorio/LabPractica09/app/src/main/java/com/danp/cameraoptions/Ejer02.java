817478d059fba3a23380c062a0ca3b71d58f4517
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2021-07-13 14:01:23 -0500

    first version ejr02

--- PracticasLaboratorio/LabPractica09/app/src/main/java/com/danp/cameraoptions/Ejer02.java
+++ PracticasLaboratorio/LabPractica09/app/src/main/java/com/danp/cameraoptions/Ejer02.java
@@ -1,14 +1,180 @@
 package com.danp.cameraoptions;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class Ejer02 extends AppCompatActivity {
+
+    private Button openCamera;
+    private ImageButton imageButton;
+    private int MY_PERMISSIONS_REQUEST_CAMARA = 0, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1;
+    private Uri _fileUri,imageurl;
+    private String currentPhotoPath;
+    private int flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejer02);
+        openCamera = findViewById(R.id.openCamera);
+        openCamera.setOnClickListener(view -> showAddFileProjectDialog());
+    }
+
+
+    private void showAddFileProjectDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Nueva Foto: ");
+        dialog.setMessage("Touch para Seleccionar una Imagen" );
+        dialog.setCancelable(false);
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        View add_layout = inflater.inflate(R.layout.data_project,null);
+        imageButton = add_layout.findViewById(R.id.addImageProject);
+        imageButton.setOnClickListener(v -> {
+            try {
+                /*Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery, "Select picture"), 4);*/
+                /*Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, 1);*/
+
+                if (Build.VERSION.SDK_INT> Build.VERSION_CODES.LOLLIPOP_MR1) {// Marshmallow+
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                            // Show an expanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+                        } else {
+                            // No se necesita dar una explicación al usuario, sólo pedimos el permiso.
+                            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMARA );
+                            // MY_PERMISSIONS_REQUEST_CAMARA es una constante definida en la app. El método callback obtiene el resultado de la petición.
+                        }
+                    }else{ //have permissions
+                        abrirCamara ();
+                    }
+                }else{ // Pre-Marshmallow
+                    abrirCamara ();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        });
+        dialog.setView(add_layout);
+        dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+
+            }
+        });
+        dialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 0: {
+                // Si la petición es cancelada, el array resultante estará vacío.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // El permiso ha sido concedido.
+                    abrirCamara();
+                } else {
+                    // Permiso denegado, deshabilita la funcionalidad que depende de este permiso.
+                    Toast.makeText(this, "Sin Permisos de Camara", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            // otros bloques de 'case' para controlar otros permisos de la aplicación
+        }
+    }
+    public void abrirCamara (){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // _fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        // i.putExtra(MediaStore.EXTRA_OUTPUT, _fileUri);
+        startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        //startActivity(i);
+        //startActivityForResult(i,0);
+
+        // ocultar();
+    }
+
+    /*public void recuperarFoto(View v) {
+        Bitmap bitmap1 = BitmapFactory.decodeFile(getExternalFilesDir(null)+"/"+et1.getText().toString());
+        imagen1.setImageBitmap(bitmap1);
+    }*/
+
+    private Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    private File getOutputMediaFile(int type) {
+        File _mediaFile;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        if (type == MEDIA_TYPE_IMAGE) {
+            _mediaFile  = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), File.separator + "IMG_" + timeStamp + ".jpg");
+        }
+        else {
+            return null;
+        }
+        return _mediaFile;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 1){
+            try{
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap mImageBitmap = (Bitmap) extras.get("data");
+                    imageButton.setImageBitmap(mImageBitmap);
+                }else if (resultCode == RESULT_CANCELED){
+                    Toast.makeText(this, "NO HAY SELECCION ALGUNA", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(this, "An error has occurred...", Toast.LENGTH_LONG).show();
+                }
+            }catch (Exception e ){
+                e.printStackTrace();
+            }
+        }
     }
 }
